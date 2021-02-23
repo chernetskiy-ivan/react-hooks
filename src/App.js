b879cf017175e075a0626ad577870d6c7cffc102
Ivan <ivanchernetskey@mail.ru>
2021-02-23 17:19:02 +0300

    linked bootstrap, useState hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  function increment(){
+    setCounter(counter +1 )
+  }
+
+  function decrement(){
+    setCounter(counter - 1 )
+  }
+
   return (
     <div>
-
+      <h1>Счетчик: {counter}</h1>
+        <button onClick={increment} className='btn btn-success'>Добавить</button>
+        <button onClick={decrement} className='btn btn-danger'>Убрать</button>
     </div>
   );
 }
