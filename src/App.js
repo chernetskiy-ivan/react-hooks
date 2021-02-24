02f7bd57e09ca8a1bb021a662fb61abdd9e4546c
Ivan <ivanchernetskey@mail.ru>
2021-02-24 15:58:44 +0300

    useRef for make focus on input element

--- src/App.js
+++ src/App.js
@@ -12,14 +12,15 @@ function App() {
 
   useEffect(() => {
     renderCount.current++
-    console.log(inputRef.current)
   })
 
+  const focus = () => inputRef.current.focus()
 
   return (
     <div>
       <h1>Количество рендеров: {renderCount.current}</h1>
       <input ref={inputRef} type='text' onChange={e => setValue(e.target.value)} value={value}/>
+      <button className='btn btn-success' onClick={focus}>Фокус</button>
     </div>
   );
 }
