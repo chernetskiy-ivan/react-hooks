d61d6f09a3c48a63af90d605026ca65bd187d894
Ivan <ivanchernetskey@mail.ru>
2021-02-24 15:29:57 +0300

    useRef for solving this problem, BUT in this case we have obj instead of [item, func] like with hook useState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
-let renderCount = 1
+//let renderCount = 1
 
 function App() {
 
   //const [renderCount, setRenderCount] = useState(1)
   const [value, setValue] = useState('initial')
+  //useRef ввозвращает объект
+  const renderCount = useRef(1)
 
   useEffect(() => {
-    renderCount++
+    renderCount.current++
   })
 
 
   return (
     <div>
-      <h1>Количество рендеров: {renderCount}</h1>
+      <h1>Количество рендеров: {renderCount.current}</h1>
       <input type='text' onChange={e => setValue(e.target.value)} value={value}/>
     </div>
   );
