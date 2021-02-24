d2cf634f58da6166e0f3c084eea38a666d212cd5
Ivan <ivanchernetskey@mail.ru>
2021-02-24 12:35:23 +0300

    Example of useing useEffect hook

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import React, {useState, useEffect} from 'react'
 
 function App() {
   const [type, setType] = useState('users')
+  const [data, setData] = useState([])
 
   // useEffect( () => {
   //   console.log('render')
   // })
 
   useEffect(() => {
-    console.log('Type change', type)
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+        .then(response => response.json())
+        .then(json => setData(json))
   }, [type])
+
+
+  useEffect(() => {
+    console.log('ComponentDidMount')
+  }, [])
 
   return (
     <div>
       <h1>Ресурс: {type}</h1>
 
       <button onClick={() => setType('users')}>Пользователи</button>
-      <button onClick={() => setType('todo')}>Todo</button>
+      <button onClick={() => setType('todos')}>Todos</button>
       <button onClick={() => setType('posts ')}>Посты</button>
+
+      <pre>{JSON.stringify(data,null,2)}</pre>
     </div>
   );
 }
