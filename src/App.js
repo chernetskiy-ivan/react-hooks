26d6d5ca79e2b47b596297b9461a8e649522e1eb
Ivan <ivanchernetskey@mail.ru>
2021-02-24 16:02:35 +0300

    useRef for save prevent value

--- src/App.js
+++ src/App.js
@@ -9,16 +9,23 @@ function App() {
   //useRef ввозвращает объект
   const renderCount = useRef(1)
   const inputRef = useRef(null)
+    const prevValue = useRef('')
 
   useEffect(() => {
     renderCount.current++
   })
+
+
+    useEffect( () => {
+        prevValue.currrent = value
+    }, [value])
 
   const focus = () => inputRef.current.focus()
 
   return (
     <div>
       <h1>Количество рендеров: {renderCount.current}</h1>
+      <h2>Прошлое состояние: {prevValue.current}</h2>
       <input ref={inputRef} type='text' onChange={e => setValue(e.target.value)} value={value}/>
       <button className='btn btn-success' onClick={focus}>Фокус</button>
     </div>
