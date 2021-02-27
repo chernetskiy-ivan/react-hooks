7ccdd9150b705f101b6d657b66642ff9b4f5fdc6
Ivan <ivanchernetskey@mail.ru>
2021-02-27 23:42:45 +0300

    useReducer and useful func

--- src/alert/AlertCotext.js
+++ src/alert/AlertCotext.js
@@ -1,25 +1,37 @@
-import React,{useState, useContext} from 'react'
+import React,{useState, useContext, useReducer} from 'react'
 
 const AlertContext = React.createContext()
-//const AlertToggleContext = React.createContext()
 
 export const useAlert = () => {
     return useContext(AlertContext)
 }
 
-// export const useAlertToggle = () => {
-//     return useContext(AlertToggleContext)
-// }
+const SHOW_ALERT = 'show'
+const HIDE_ALERT = 'hide'
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case SHOW_ALERT: return {...state, visible: true, text: action.text}
+        case HIDE_ALERT: return {...state, visible: false}
+        default: return state
+    }
+}
 
 export const AlertProvider = ({children}) => {
+    const [state, dispatch] = useReducer(reducer, {
+        visible: false,
+        text: ''
+    } )
 
-    const [alert, setAlert]= useState(false)
-    const toggle = () => setAlert(prev => !prev)
+    const show = text => dispatch({type: SHOW_ALERT, text})
+    const hide = () => dispatch({type: HIDE_ALERT})
 
     return(
         <AlertContext.Provider value={{
-            visible: alert,
-            toggle: toggle
+            visible: state.visible,
+            show: show,
+            hide: hide,
+            text: state.text
         }}>
             {children}
         </AlertContext.Provider>
